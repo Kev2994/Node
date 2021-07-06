3004c1aa680043b7122f5ba8addc4c6fae798c99
Kevin Virani <kevinvirani5@gmail.com>
2021-07-06 15:12:21 -0400

    Added

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 // JavaScript source code
 const express = require('express');
 const app = express();
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
 var obj;
 fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => obj = data);
 
 app.get('/getUser/:username', (req, res) => {
-    
+
     const userInfo = obj.find(el => el.username === req.params.username);
     res.status(200).json(userInfo);
-    
-})
 
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+    console.log(`listening on port ${port}`);
+});
